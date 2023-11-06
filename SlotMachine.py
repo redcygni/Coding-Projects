940ffe20c45c317deda59a123661599fc5c59db1
Aidan Lawlor <118145890+redcygni@users.noreply.github.com>
2023-11-06 17:10:26 -0500

    Update SlotMachine.py

--- SlotMachine.py
+++ SlotMachine.py
@@ -19,7 +19,7 @@ class Slots:
         self.symbols = symbols
         self.balance = balance
 
-    def Spin(self, bet):
+    def spin(self, bet):
         if bet <= 0 or bet > self.balance:
             print("Invalid bet. Please bet a positive amount within your balance.")
             return
@@ -31,32 +31,32 @@ class Slots:
             result[i].printSymbol()
         print(" |")
         print("-----------\n")
-
-        if result[0].value == result[1].value == result[2].value:
+        true = True
+        if (result[0].value == result[1].value == result[2].value):
             print("You won!")
             self.balance += bet * result[0].weight
         else:
             print("You lost.")
             self.balance -= bet
 
-print("Welcome to the slot machine!")
-run = True
-Balance = 1000
-    
-while run:
-    bet = int(input("How much would you like to bet?: $"))
-    round = Slots(symbol_list, Balance)
-    
-    if Balance <= 0:
-        print("You ran out of money. Thanks for playing!")
-        run = False
-        continue
-    
-    round.Spin(bet)
-    if round.balance > 0:
-        Balance = round.balance
-        print("Your remaining balance is: $" + str(Balance))
-        cont = input("Continue? (Y/N): ")
-        if cont.lower() == "n":
-            print("Thanks for playing.")
+print("Welcome to the slot machine! Your starting balance is $1000.")
+def main():    
+    run = True
+    Balance = 1000
+    while run == True:
+        bet = int(input("How much would you like to bet?: $"))
+        round = Slots(symbol_list, Balance)
+        round.spin(bet)
+        if round.balance <= 0:
+            print("You ran out of money. Thanks for playing!")
             run = False
+            break
+        
+        if round.balance > 0:
+            Balance = round.balance
+            print("Your remaining balance is: $" + str(Balance))
+            cont = input("Continue? (Y/N): ")
+            if cont.lower() == "n":
+                print("Thanks for playing.")
+                run = False
+main()
